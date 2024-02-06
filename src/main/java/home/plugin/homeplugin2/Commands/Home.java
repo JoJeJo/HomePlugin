758b6fa5d2c8418530a3f9a9e77e19a493a92fb5
JoJeJo <hijeromj@gmail.com>
2024-02-06 12:31:06 -0800

    Added comments. Fixed countdown. Fixed cooldown.

--- src/main/java/home/plugin/homeplugin2/Commands/Home.java
+++ src/main/java/home/plugin/homeplugin2/Commands/Home.java
@@ -26,17 +26,19 @@ public class Home implements CommandExecutor {
         Player player = null;
         if (command.getName().equalsIgnoreCase("home")) {
             player = (Player) sender;
-
+            // If player has no bed spawn set
             if (player.getBedSpawnLocation() == null) {
                 sender.sendMessage("§eYou have no set spawn. Right-Click a bed to set your spawn.");
                 cooldown.put(player.getUniqueId(), System.currentTimeMillis() - 10000);
                 return false;
             }
+            // Can not use command in Nether
             if (player.getWorld().getName().equals("world_nether")) {
                 sender.sendMessage("§eYou can not use /home in the Nether.");
                 cooldown.put(player.getUniqueId(), System.currentTimeMillis() - 10000);
                 return false;
             }
+            // Can not use command in End
             if (player.getWorld().getName().equals("world_the_end")) {
                 sender.sendMessage("§eYou can not use /home in the End.");
                 cooldown.put(player.getUniqueId(), System.currentTimeMillis() - 10000);
@@ -45,6 +47,7 @@ public class Home implements CommandExecutor {
             if (!cooldown.containsKey(player.getUniqueId()) || System.currentTimeMillis() - cooldown.get(player.getUniqueId()) > 10000) {
                 cooldown.put(player.getUniqueId(), System.currentTimeMillis());
                 try {
+                    //countdown
                     for (int i = 0; i < 5; i++) {
                         double health1 = player.getHealth();
                         @NotNull Location location1 = player.getLocation();
@@ -52,23 +55,28 @@ public class Home implements CommandExecutor {
                         Thread.sleep(1000);
                         double health2 = player.getHealth();
                         @NotNull Location location2 = player.getLocation();
+                        // If player takes damage countdown stops
                         if (!((health1 == health2))) {
                             sender.sendMessage("You can not take damage when teleporting home.");
                             return false;
                         }
-                        if (!((location1 == location2))) {
+                        // If player moves countdown stops
+                        /* if (location1 != location2) {
                             sender.sendMessage("You need to stand still when teleporting home.");
                             return false;
-                        }
+                        } */
                     }
+                    // Teleport
                     player.teleportAsync(player.getBedSpawnLocation());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                return true;
             }
-        } else
-            sender.sendMessage("§eYou can't teleport home for another " + (10 - ((System.currentTimeMillis() - cooldown.get(player.getUniqueId())) / 1000)) + " seconds!");
-        return true;
+            else {
+                sender.sendMessage("§eYou can't teleport home for another " + (10 - ((System.currentTimeMillis() - cooldown.get(player.getUniqueId())) / 1000)) + " seconds!");
+                return false;
+                }
+        }
+    return true;
     }
 }
