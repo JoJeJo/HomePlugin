bc3f473dc11bf980f8820306fc9b45a7f8cb0af2
JoJeJo <hijeromj@gmail.com>
2024-02-05 21:42:50 -0800

    Added messages when /home can't be used in nether and home

--- src/main/java/home/plugin/homeplugin2/Commands/Home.java
+++ src/main/java/home/plugin/homeplugin2/Commands/Home.java
@@ -1,5 +1,6 @@
 package home.plugin.homeplugin2.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,11 +15,18 @@ public class Home implements CommandExecutor{
             if (player.getBedSpawnLocation() == null) {
                 return false;
             }
-            if (player.getWorld().getName() == player.getBedSpawnLocation().getWorld().getName()) {
+            if (player.getWorld().getName().equals(player.getBedSpawnLocation().getWorld().getName())) {
                 player.teleportAsync(player.getBedSpawnLocation());
                 return true;
             }
+            if (player.getWorld().getName().equals("world_nether")) {
+                sender.sendMessage("§eYou can not use /home in the Nether");
+            }
+            if (player.getWorld().getName().equals("world_the_end")) {
+                sender.sendMessage("§eYou can not use /home in the End");
+            }
         }
+
         return false;
     }
 
--- src/main/java/home/plugin/homeplugin2/HomePlugin2.java
+++ src/main/java/home/plugin/homeplugin2/HomePlugin2.java
@@ -1,6 +1,6 @@
 package home.plugin.homeplugin2;
-package org.bukkit.entity;
 
+import home.plugin.homeplugin2.Commands.Home;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HomePlugin2 extends JavaPlugin {
@@ -9,7 +9,7 @@ public final class HomePlugin2 extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
-
+        getCommand("home").setExecutor(new Home());
     }
     @Override
     public void onDisable() {
